9838fc13b6ac956e648535b29002a58626d880d4
Kyle Johnsen <kaelstrom@gmail.com>
2012-12-01 17:53:17 -0500

    machine generates grid of dummy tiles

--- src/machine.cpp
+++ src/machine.cpp
@@ -1,1 +1,41 @@
 #include "machine.h"
+
+Machine::Machine(float x_offset_, float y_offset_, float width_, float height_, float tile_buffer_, int grid_width_, int grid_height_) {
+
+	//drawing dimensions of overall machine
+	x_offset = x_offset_;
+	y_offset = y_offset_;
+	width = width_;
+	height = height_;
+
+	//space between tiles
+	tile_buffer = tile_buffer_;
+
+	//number of elements in the grid along width and height
+	grid_width = grid_width_;
+	grid_height = grid_height_;
+	
+	// determine size of a single tile by removing buffer space from machine size and dividing whats left
+	tile_width  = (width -  (tile_buffer * grid_width-1))  / grid_width;
+	tile_height = (height - (tile_buffer * grid_height-1)) / grid_height;
+}
+
+// fills the machine with a grid of dummy tiles
+void Machine::fillWithDummy() {
+
+	// loop for each row
+	for(int y = 0; y < grid_height; y++) {
+		vector<Tile*> row;
+
+		//loop for each element in the row
+		for(int x = 0; x < grid_width; x++) {
+			Tile * tmp = new DummyTile;
+			tmp->setPosition(x_offset+x*(tile_buffer+tile_width), y_offset+y*(tile_buffer+tile_height));
+			tmp->setWidth(tile_width);
+			tmp->setHeight(tile_height);
+			row.push_back(tmp);
+		}
+
+		grid.push_back(row);
+	}
+}
--- src/machine.h
+++ src/machine.h
@@ -1,5 +1,15 @@
 // machine class that generates and holds the grid of tiles to be projected
+#include "tile.h"
+#include <vector>
+using namespace std;
 
-class machine {
+class Machine {
+private:
+	float x_offset, y_offset, width, height, tile_buffer, tile_width, tile_height;
+	int grid_width, grid_height;
+	vector<vector<Tile*>> grid;
+public:
+	Machine(float x_offset, float y_offset, float width, float height, float tile_buffer, int grid_width, int grid_height);
+	void fillWithDummy();
 
 };
